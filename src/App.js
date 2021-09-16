8221ebb918bb7d407586ed66cafcba6d51b271a9
troyclarke69 <teclarke@rogers.com>
2021-09-16 16:19:04 -0400

    09-16-21 updates

--- src/App.js
+++ src/App.js
@@ -9,28 +9,13 @@ import Stats from './pages/Stats';
 
 function App() {
 
-  // just a test ...
-  // const [counter, setCounter] = useState(0);
-  // const add = () => {
-  //   // NOTE: counter++ does not work here == receive 'assignment to constant variable'
-  //   setCounter(counter + 1);
-  // }
-
-
   return(
-
-    // <div className='App dark'>
     <>
     <div className='App'>
-      {/* <button onClick={() => setCounter(counter + 1)}>Add</button> */}
-      {/* <button onClick={() => add()}>Add</button> */}
-      {/* <h2>{counter}</h2> */}
-
       <Switch>
         <Route exact path="/" component={Splash}/>
         <Route exact path="/Home" component={Home}/>
         <Route exact path="/Stats" component={Stats}/>
-        {/* <Route exact path="/countries/:id" component={Countries}/> */}
         <Route exact path="/earth" component={Earth}/>
         <Route component={Error}/>
       </Switch>
